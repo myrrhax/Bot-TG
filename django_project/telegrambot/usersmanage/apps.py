62173186c6b860ee9a9336c3ecc38637be765c72
myrrhax <80592551+myrrhax@users.noreply.github.com>
2021-06-03 22:53:49 +0300

    Delete django_project directory

--- django_project/telegrambot/usersmanage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersmanageConfig(AppConfig):
-    #  default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_project.telegrambot.usersmanage'
