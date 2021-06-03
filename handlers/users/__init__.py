34695a4f354b3010b357950d10b0b395a6e5a228
myrrhax <80592551+myrrhax@users.noreply.github.com>
2021-06-03 22:54:03 +0300

    Delete handlers directory

--- handlers/users/__init__.py
+++
@@ -1,9 +0,0 @@
-from .help import dp
-from .start import dp
-from .item_answer import dp
-from .admin_panel import dp
-from .buying_item import dp
-from .referral_system_and_cancel import dp
-from .promo import dp
-
-__all__ = ["dp"]
