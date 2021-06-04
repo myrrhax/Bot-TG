834605c99a6e99ad89f4eb20e1ca80f13c470404
myrrhax <80592551+myrrhax@users.noreply.github.com>
2021-06-04 15:42:48 +0300

    Update settings.py

--- django_project/telegrambot/telegrambot/settings.py
+++ django_project/telegrambot/telegrambot/settings.py
@@ -2,7 +2,7 @@ import os
 from data import config
 import sys
 
-sys.path.append('C:/Users/myrrhax/Desktop/final_project')
+sys.path.append('YOURPATH')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
